088a64742d0ab7c749e77d99d604291f2ef93726
sophiecaroni <sophie.caroni@gmail.com>
2023-04-25 16:00:19 +0200

    review of analytics class

--- analytics/analytics_graph.py
+++ analytics/analytics_graph.py
@@ -17,11 +17,11 @@ class AnalyticsGraph:
     def centrality_betweenness_library(graph):
         """
         Creator : Quentin Nater
-        reviewed by :
-        Calculate the betweenness centrality of a graph using the networkx library
-        :param graph: networkX - Graph networkX of the amazon dataset
+        reviewed by : Sophie Caroni
+        Calculate the betweenness centrality of a graph using the networkX library
+        :param graph: networkX - Graph networkX (of the amazon dataset refined)
         :type graph: networkX
-        :return: nodes with the betweenness centrality
+        :return: Each node with its betweenness centrality value (if above 0.0)
         """
         current_time = datetime.datetime.now()
         print(">> You have called the betweenness centrality library for your graph (at", current_time, ").")
@@ -29,24 +29,28 @@ class AnalyticsGraph:
         nodes = nx.betweenness_centrality(
             graph)  # Centrality dictionary: node as key and its betweenness centrality as value
 
+        non_central_nodes = 0
         for node in nodes.keys():
             if round(nodes[node], 2) > 0.0:
                 print("\t\t\t\t" + str(node) + " : " + str(round(nodes[node], 3)))
-        else:
-            print("\t\t\t\t Result: There are no central nodes.")
+            else:
+                non_central_nodes += 1
+
+        if non_central_nodes == len(nodes.keys()):
+            print("\t\t\t\tResult: There are no nodes having a betweenness centrality above 0.0.")
 
         return nodes
 
     def community_library_detection(graph, library="Default"):
         """
         Creator : Quentin Nater
-        reviewed by :
+        reviewed by : Sophie Caroni
         Detect communities with a specific library
-        :param graph: networkX - Graph networkX of the amazon dataset
+        :param graph: networkX - Graph networkX (of the amazon dataset refined)
         :type graph: networkX
-        :param library: String - Library chosen (girvanNewman;louvain;modularity)
-        :type graph: String
-        :return: Communities found
+        :param library: string - Library chosen (girvanNewman;louvain;modularity)
+        :type graph: string
+        :return: Communities found by the chosen library
         """
         current_time = datetime.datetime.now()
         print(">> You have called the community detection with ", library, " (at", current_time, ")")
@@ -56,9 +60,11 @@ class AnalyticsGraph:
         # Library: community.girvan_newman
         if library == "girvanNewman":
             communities = girvan_newman(graph)
+            print(communities)
             for i, c in enumerate(communities):
                 print("(girvanNewman) > community ", i, " : ", c)
 
+            ### throws an error (using flag = prod and graph=graph) - Sophie
             modularity_score = modularity(graph, communities)
             print("Modularity score:", modularity_score)
 
@@ -86,29 +92,29 @@ class AnalyticsGraph:
     def homemade_modularity_gain(graph, first_community, second_community):
         """
         Creator : Sophie Caroni
-        reviewed by :
+        reviewed by : Sophie Caroni
         Compute modularity gain between two communities
-        :param graph: networkX - Graph networkX of the amazon dataset (rafined)
+        :param graph: networkX - Graph networkX (of the amazon dataset refined)
         :type graph: networkX
         :param first_community: list of nodes belonging to the first community
         :type first_community: list
         :param second_community: list of nodes belonging to the first community
         :type second_community: list
-        :return: modularity gain with first and second communities
+        :return: Modularity gain, first community and second community
         """
         n_edges = nx.number_of_edges(graph)
 
-        # find number of neighbors of each node inside the first community
+        # Find number of neighbors of each node inside the first community
         d_i = 0
         for n in first_community:
             d_i += len(list(graph.neighbors(n)))
 
-        # find number of neighbors of each node inside the second community
+        # Find number of neighbors of each node inside the second community
         d_j = 0
         for n in second_community:
             d_j += len(list(graph.neighbors(n)))
 
-        # find number of shared links between first and second community
+        # Find number of shared links between first and second community
         d_ij = 0
         for n in first_community:
             for ni in graph.neighbors(n):
@@ -120,7 +126,7 @@ class AnalyticsGraph:
                 if mi in first_community:
                     d_ij += 1
 
-        # compute modularity gain
+        # Compute modularity gain between the two communities
         modularity_gain = 1 / (2 * n_edges) * (d_ij - (d_i * d_j / n_edges))
 
         return [modularity_gain, first_community, second_community]
@@ -128,19 +134,20 @@ class AnalyticsGraph:
     def homemade_community_detection(graph, display=False):
         """
         Creator : Quentin Nater
-        reviewed by :
-        Detect communities with a specific algo
-        :param graph: networkX - Graph networkX of the amazon dataset (rafined)
+        reviewed by : Sophie Caroni
+        Detect communities with a specific algorithm
+        :param graph: networkX - Graph networkX (of the amazon dataset refined)
         :type graph: networkX
-        :return: Communities found
+        :return: Communities found based on the best modularity gain
         """
         current_time = datetime.datetime.now()
         print(">> You've called the homemade (good choice) community detection (at", current_time, "), please wait <3")
 
         # phase 1 =====================================================================================================
+        # Set initial communities as one node each and compute the initial modularity
         communities = [[community] for community in graph.nodes()]
-        best_modularity = modularity(nx.Graph(graph), communities)
-        original_modularity = best_modularity
+        best_modularity = modularity(nx.Graph(graph), communities) ### should't be original_modularity instead and
+        original_modularity = best_modularity                      ### here the best ? btw would change to initial_modularity - Sophie
 
         if display:
             print("\tOriginal community list : ", communities)
@@ -150,18 +157,22 @@ class AnalyticsGraph:
 
         while limit > inc:
             # phase 2 =====================================================================================================
+            # Retrieve the first community
             community_vi = communities.pop(0)
 
             if display:
                 print("\t\tcommunity_vi :\t", community_vi)
 
             # phase 3 =====================================================================================================
-            allResult, neighbors, communityNeighbors = [], [], []
+            allResult, neighbors, communityNeighbors = [], [], [] ### maybe need to better define the last two (what is the difference?) - Sophie
 
+            # Retrieve all neighbors of this first community (i.e. of each node of the community)
             for node in community_vi:
                 for n in graph.neighbors(node):
                     neighbors.append(n)
 
+            # Retrieve all non-common (?) neighbors between this first community and all other communities (?)
+            ### not sure about this, need to double check - Sophie
             for n in neighbors:
                 for community in communities:
                     if n in community:
@@ -173,17 +184,21 @@ class AnalyticsGraph:
             if len(communityNeighbors) == 0:
                 communities.append(community_vi)
                 inc += 1
-                continue
+                continue ### is this really needed? - Sophie
 
+            # Compute modularity gain for each of the communities born by adding community_vi to each of them
             for community in communityNeighbors:
                 allResult.append(AnalyticsGraph.homemade_modularity_gain(graph, community_vi, community))
 
+            # Save as "maxValue" the highest modularity gain brought by the adding of community_vi
             maxValue = max(allResult[n][0] for n in range(len(allResult)))
 
+            # Save as "winningCommunity" the one getting the highest modularity gain from the adding of community_vi
             for i, values in enumerate(allResult):
                 if allResult[i][0] == maxValue:
                     winningCommunity = allResult[i][2]
 
+            ### not sure about what does this - Sophie
             if maxValue > 0:
                 for community in communities:
                     if community == winningCommunity:
@@ -202,6 +217,7 @@ class AnalyticsGraph:
                 print("\t\tscore : ", current_modularity)
                 print("\t\thigh score : ", best_modularity)
 
+            ### not sure about what does this - Sophie
             if current_modularity < best_modularity > 0:
                 break
             else:
@@ -226,16 +242,29 @@ class AnalyticsGraph:
         return communities
 
     def compare_algo_efficiency(graph, communities_algo_homemade):
+        """
+        Creator : Quentin Nater
+        reviewed by : Sophie Caroni
+        Compare communities found using homemade algorithm with those found using louvain_communities library
+        :param graph: networkX - Graph networkX (of the amazon dataset refined)
+        :type graph: networkX
+        :param communities_algo_homemade: list of communities, which are list of nodes
+        :type communities_algo_homemade: list of list
+        :return: Normalized Mutual Information between the two communities
+        """
         print(">> You've called the comparator of algorithm, please wait :)")
 
+        # Compute the communities of the graph using louvain_community library
         communities_algo_library = louvain_communities(graph, seed=123)
 
+        # Compute modularities of the graph for homemade versus with library fund communities
         modularity_homemade = modularity(graph, communities_algo_homemade)
         modularity_library = modularity(graph, communities_algo_library)
 
         print("\t\tmodularity 1st algorithm :", modularity_homemade)
         print("\t\tmodularity 2nd algorithm :", modularity_library)
 
+        # Compare the communities, if they are the same number
         try:
             labels_true = [0] * len(communities_algo_homemade[0]) + [1] * len(communities_algo_homemade[1])
             labels_pred = [0] * len(communities_algo_library[0]) + [1] * len(communities_algo_library[1])
@@ -244,6 +273,6 @@ class AnalyticsGraph:
             print("\t\tNMI3 score :", round((nmi * 100), 2), "%")
         except:
             nmi = -1
-            print("\t\tCannot get score if the number communities are not the same...")
+            print("\t\tCannot get a NMI score if the number communities are not the same...")
 
         return nmi
--- project_graph.py
+++ project_graph.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     if os.name == "nt":
         mpl.use('TkAgg')  # without it, cannot run my plots (maybe personal)
     elif os.name == "posix":
-        print("Sorry bro about your MAC...")
+        print("Choosing a Mac really is the best thing you've done in life!")
     else:
         print("Unknown operating system.")
 
@@ -69,10 +69,12 @@ if __name__ == '__main__':
                               ("6", "7"),("6", "8")])
 
 
-        graph3 = eg.construct_graph_by_file("./dataset/amazon_refined.txt")
-        pg.remove_nodes_by_degree(graph3, 5)
+        # graph3 = eg.construct_graph_by_file("./dataset/amazon_refined.txt")
+        # pg.remove_nodes_by_degree(graph3, 5)
 
-        vg.display_simple_graph(graph, False)
+        # vg.display_simple_graph(graph2, display=False)
+        # ag.community_library_detection(graph2, library="modularity")
 
-        communities = ag.homemade_community_detection(graph3)
-        ag.compare_algo_efficiency(graph3, communities)
+        communities = ag.homemade_community_detection(graph)
+        ag.compare_algo_efficiency(graph, communities)
+        # ag.compare_algo_efficiency(graph3, communities)
