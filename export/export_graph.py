af01f40280fe2542b237b533f9f36929eff51e50
sophiecaroni <sophie.caroni@gmail.com>
2023-04-27 19:14:04 +0200

    sophie export txt to csv (to improve ...)

--- export/export_graph.py
+++ export/export_graph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from explore.exploration_graph import ExploreGraph as eg
+import csv
 
 
 class ExportGraph:
@@ -44,6 +45,49 @@ class ExportGraph:
                 file.write('\n')
 
         print(">> You can find your refined graph in this directory './dataset/', please enjoy ;)")
+
+        return
+
+    def txt_to_csv(input_file, output_file):
+        """
+        Creator : Sophie Caroni
+        reviewed by :
+        Create a .csv file of the txt amazon_refined dataset
+        :param input_file: file of the amazon_refined dataset
+        :type input_file: .txt
+        :return: -
+        """
+
+        print(">> You have called the conversion in csv of your dataset, please wait :)")
+
+        # Open the input txt file for reading and the output csv file for writing
+        with open(input_file, 'r') as txt_file, open(output_file, 'w', newline='') as csv_file:
+
+            # Create a csv writer object and write the header row to the csv file
+            writer = csv.writer(csv_file)
+            writer.writerow(['ASIN', 'SIMILAR_TO'])
+
+            # Initialize a variable to keep track of the current ASIN being processed
+            current_asin = None
+
+            # Loop through each line in the input txt file
+            for line in txt_file:
+
+                # Strip any leading or trailing whitespace from the line
+                line = line.strip()
+
+                # If the line starts with 'ASIN:', extract the ASIN and set it as the current ASIN
+                if line.startswith('ASIN:'):
+                    current_asin = line.split()[1]
+
+                # If the line starts with 'similar:', extract the similar ASINs and write them to the csv file
+                elif line.startswith('similar:'):
+
+                    # Extract the similar ASINs (by skipping the two first elements, i.e. "similar:" and nr of similars)
+                    similar_asins = line.split()[2:]
+
+                    # Write the current ASIN and the list of similar ASINs to the csv file
+                    writer.writerow([current_asin, *similar_asins])
 
         return
 
